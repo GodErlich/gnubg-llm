f3b73bc1cf0ef788b2022b3b892739cbc9f462ad
Alon <aloncvb@gmail.com>
2025-06-09 01:03:41 +0300

    remove old images

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,7 +1,8 @@
 @echo off
 echo Building gnubg container...
 docker build -t gnubg-app .
-
+echo Cleaning up old images...
+docker image prune -f
 echo Running gnubg container...
 docker run -it --rm ^
     -v %cd%\output:/app/output ^
