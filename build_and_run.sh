c552e31c5a3c05942f3d7dc4f835428283e31c91
Alon <aloncvb@proton.me>
2025-08-09 15:40:34 +0300

    remove docker

--- build_and_run.sh
+++
@@ -1,8 +0,0 @@
-echo "Building gnubg container..."
-docker build -t gnubg-app .
-
-echo "Running gnubg container..."
-docker run -it --rm \
-    -v $(pwd)/output:/app/output \
-    -v $(pwd)/input:/app/input \
-    gnubg-app
