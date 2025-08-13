3e4f166882a9a21d78f628477b3226586520685d
Alon <aloncvb@proton.me>
2025-08-14 02:10:51 +0300

    remove and note

--- src/interfaces.py
+++ src/interfaces.py
@@ -4,12 +4,6 @@ class Hint(TypedDict):
     move: str
     equity: int
 
-class CubeDecision(TypedDict):
-    decision_type: str  # "double_decision" or "take_decision"
-    can_double: bool
-    cube_value: int
-    cube_owner: Optional[str]
-
 class AgentInputConfig(TypedDict):
     possible_moves: bool
     hints: bool
