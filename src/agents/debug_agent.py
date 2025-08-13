006310b6b9285f109aa98341ad556fa785529480
Alon <aloncvb@proton.me>
2025-08-13 13:01:24 +0300

    change debug agent

--- src/agents/debug_agent.py
+++ src/agents/debug_agent.py
@@ -4,20 +4,14 @@ from ..utils import random_move
 from ..logger import logger
 
 
-class DebugAgent(Agent):
-    """Agent for debugging purposes, does not select moves but logs information."""
+class BestMoveAgent(Agent):
+    """Agent that always picks the "best move" according to gnubg engine"""
 
     def __init__(self, board_representation=None, inputs: AgentInputConfig = {}):
         super().__init__(board_representation, inputs)
 
     def choose_move(self, board, extra_input: AgentInput = None):
         """ choose a random move from the possible moves """
-        possible_moves = extra_input.get("possible_moves", [])
-        hints = extra_input.get("hints", [])
         best_move = extra_input.get("best_move", None)
-        logger.debug(f"{self.board_representation()}")
-        logger.debug(f"Possible Moves: {possible_moves}")
-        logger.debug(f"Hints: {hints}")
         logger.debug(f"Best Move: {best_move}")
-        move = random_move()
-        return move
+        return best_move
